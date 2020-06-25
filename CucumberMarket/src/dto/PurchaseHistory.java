36b01d68ea90090a913883aef21979db4a98bfdf
5d5ng <deo1915@gmail.com>
2020-06-25 22:05:18 +0900

    오이마켓

---
+++ CucumberMarket/src/dto/PurchaseHistory.java
@@ -0,0 +1,46 @@
+package dto;
+
+public class PurchaseHistory {
+	
+	private int HistoryNumber;
+	private String CustomerID;
+	private int TotalCost;
+	private String PaymentStatus;
+	public int getHistoryNumber() {
+		return HistoryNumber;
+	}
+	public void setHistoryNumber(int historyNumber) {
+		HistoryNumber = historyNumber;
+	}
+	public String getCustomerID() {
+		return CustomerID;
+	}
+	public void setCustomerID(String customerID) {
+		CustomerID = customerID;
+	}
+	public int getTotalCost() {
+		return TotalCost;
+	}
+	public void setTotalCost(int totalCost) {
+		TotalCost = totalCost;
+	}
+	public String getPaymentStatus() {
+		return PaymentStatus;
+	}
+	public void setPaymentStatus(String paymentStatus) {
+		PaymentStatus = paymentStatus;
+	}
+	public PurchaseHistory(int historyNumber, String customerID, int totalCost, String paymentStatus) {
+		super();
+		HistoryNumber = historyNumber;
+		CustomerID = customerID;
+		TotalCost = totalCost;
+		PaymentStatus = paymentStatus;
+	}
+	@Override
+	public String toString() {
+		return "PurchaseHistory [HistoryNumber=" + HistoryNumber + ", CustomerID=" + CustomerID + ", TotalCost="
+				+ TotalCost + ", PaymentStatus=" + PaymentStatus + "]";
+	}
+	
+}
---
+++ CucumberMarket/src/test/Test.java
@@ -0,0 +1,22 @@
+package test;
+import java.util.*;
+import dao.*;
+import dto.*;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CustomerDao dao = new CustomerDao();
+		dao.getCustomers();
+		DeliveryDao Ddao = new DeliveryDao();
+		ProductDao Pdao = new ProductDao();
+		PurchasHistoryDao PHdao = new PurchasHistoryDao();
+		OrderProductDao OPdao = new OrderProductDao();
+		Pdao.deleteProduct(400);
+		//OPdao.updateOrderProduct(1);
+//		PHdao.updatePurchaseHisory(11231, 1);
+		PHdao.deletePurchaseHistory(2);
+
+	}
+
+}
